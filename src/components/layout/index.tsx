3cfd2b87e92fa3e932249e979a173c73140d2a1b
Ben Akram <bakram4@myseneca.ca>
2025-03-17 15:42:46 -0400

    fix- account side bar styling

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -3,11 +3,12 @@ import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
 import { walmartKeys } from "@/hooks";
 import { getQueryClient } from "@/providers/get-query-client";
 
-import MainNav from "./main-nav";
+import Footer from "../landing-page/footer";
+import SiteHeader from "./main-nav";
 
 const baseUrl = process.env.NEXT_PUBLIC_PROD_URL || "http://localhost:3000";
 
-export async function Layout() {
+export async function Layout({ children }: { children: React.ReactNode }) {
 	const queryClient = getQueryClient();
 	await queryClient.prefetchQuery({
 		queryKey: walmartKeys.categories(),
@@ -26,7 +27,9 @@ export async function Layout() {
 	// Hydrate the pre-fetched state to the client
 	return (
 		<HydrationBoundary state={dehydrate(queryClient)}>
-			<MainNav />
+			<SiteHeader />
+			{children}
+			<Footer />
 		</HydrationBoundary>
 	);
 }
